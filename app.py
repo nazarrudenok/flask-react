acc40869fc849d308359204825a7989f890f0cdf
Nazar Rudenok <122445773+nazarrudenok@users.noreply.github.com>
2024-01-14 23:15:32 +0200

    Add files via upload

--- app.py
+++ app.py
@@ -1,22 +1,7 @@
 from flask import Flask, render_template, request
-import pymysql
 import random
 
 app = Flask(__name__)
-
-app.config['MYSQL_HOST'] = 'localhost'
-app.config['MYSQL_USER'] = 'root'
-app.config['MYSQL_PASSWORD'] = ''
-app.config['MYSQL_DB'] = 'test228'
-
-connection = pymysql.connect(
-    host=app.config['MYSQL_HOST'],
-    user=app.config['MYSQL_USER'],
-    password=app.config['MYSQL_PASSWORD'],
-    db=app.config['MYSQL_DB']
-)
-
-cursor = connection.cursor()
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
